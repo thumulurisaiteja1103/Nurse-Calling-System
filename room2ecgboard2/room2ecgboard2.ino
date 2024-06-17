1a5d5cb2ffdba1024c585afb687d2638ec13fa13
thumulurisaiteja1103 <thumulurisaiteja2004@gmail.com>
2024-06-17 21:43:20 +0530

    initial Commit

---
+++ room2ecgboard2/room2ecgboard2.ino
@@ -0,0 +1,138 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+const char* ssid = "SRKR_IDEALAB";
+const char* password = "Tech$9889";
+const char* mqtt_server = "192.168.68.182"; // Local IP address of Raspberry Pi
+const char* username = "";
+const char* pass = "";
+
+#define Relay1   D0
+#define Relay2   D1
+#define IRPIN1   D4
+#define IRPIN2   D3
+#define sensor   A0
+#define loplus   D5
+#define lominu   D6
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define sub1 "device2/relay1"
+#define sub2 "device2/relay2"
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  if (strstr(topic, sub1)) {
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    if ((char)payload[0] == '1') {
+      digitalWrite(Relay1, HIGH);
+    } else {
+      digitalWrite(Relay1, LOW);
+    }
+  }
+  else if (strstr(topic, sub2)) {
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+    if ((char)payload[0] == '1') {
+      digitalWrite(Relay2, HIGH);
+    } else {
+      digitalWrite(Relay2, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str(), username, pass)) {
+      Serial.println("connected");
+      client.subscribe(sub1);
+      client.subscribe(sub2);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
+  pinMode(IRPIN1, INPUT);
+  pinMode(IRPIN2, INPUT);
+  pinMode(loplus, INPUT);
+  pinMode(lominu, INPUT);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  static unsigned long lastMsg = 0;
+  if (now - lastMsg > 2000) {
+    int bs1 = digitalRead(IRPIN1);
+    int bs2 = digitalRead(IRPIN2);
+    int Svalue = analogRead(sensor) / 15;
+
+    // Read ECG sensor value (placeholder code)
+    int ecgValue = analogRead(A0);
+
+    lastMsg = now;
+    char str_ir_data1[10];
+    char str_ir_data2[10];
+    char str_ecg_value[10];
+   
+    dtostrf(bs1, 4, 4, str_ir_data1);
+    dtostrf(bs2, 4, 4, str_ir_data2);
+    dtostrf(ecgValue, 4, 4, str_ecg_value);
+
+    Serial.print("ir1 - ");
+    Serial.println(str_ir_data1);
+    client.publish("device2/call1", str_ir_data1);
+    Serial.print("ir2 - ");
+    Serial.println(str_ir_data2);
+    client.publish("device2/call2", str_ir_data2);
+
+    Serial.print("ECG Value - ");
+    Serial.println(str_ecg_value);
+    client.publish("device2/ecg", str_ecg_value);
+  }
+}
---
+++ room2tempand_bpm/room2tempand_bpm.ino
@@ -0,0 +1,97 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define sensor A0
+int Svalue;
+const char* ssid = "SRKR_IDEALAB";
+const char* password = "Tech$9889";
+const char* mqtt_server = "192.168.68.182";  // Local IP address of Raspberry Pi
+const char* username = "";
+const char* pass = "";
+
+char str_temp_cel[10];
+char str_temp_faren[10];
+char str_heart_data[10];
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+const int oneWireBus = 4;     
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str(), username, pass)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883); // Start the DS18B20 sensor
+  sensors.begin();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    sensors.requestTemperatures(); 
+    float temperatureC = sensors.getTempCByIndex(0);
+    dtostrf(temperatureC, 4, 2, str_temp_cel);
+    float temperatureF = sensors.getTempFByIndex(0);
+    dtostrf(temperatureF, 4, 2, str_temp_faren);
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.print("ºC - ");
+    Serial.print(temperatureF);
+    Serial.println("ºF");
+    Svalue = analogRead(sensor) / 14;
+    dtostrf(Svalue, 4, 4, str_heart_data);
+    lastMsg = now;
+    Serial.print("Publish message: ");
+    Serial.print("Heartbeat - "); 
+    Serial.println(str_heart_data);
+    client.publish("device2/bpm", str_heart_data);
+    client.publish("device2/tempc", str_temp_cel);
+    client.publish("device2/tempF", str_temp_faren);
+  }
+}
